7f77a88aa002c5dc7481b8d93c09001b767519f8
sho kisaragi <sho@oshiire.to>
2024-06-03 21:56:38 +0900

    first commit

---
+++ wezterm.lua
@@ -0,0 +1,284 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+-- This table will hold the configuration.
+local config = {}
+
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- make task numbers clickable
+-- the first matched regex group is captured in $1.
+table.insert(config.hyperlink_rules, {
+  regex = [[\b[tt](\d+)\b]],
+  format = 'https://example.com/tasks/?t=$1',
+})
+
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+-- as long as a full url hyperlink regex exists above this it should not match a full url to
+-- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+table.insert(config.hyperlink_rules, {
+  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+  format = 'https://www.github.com/$1/$3',
+})
+
+-- Right status bar
+wezterm.on('update-right-status', function(window, pane)
+  -- Each element holds the text for a cell in a "powerline" style << fade
+  local cells = {}
+
+  -- Figure out the cwd and host of the current pane.
+  -- This will pick up the hostname for the remote host if your
+  -- shell is using OSC 7 on the remote host.
+  local cwd_uri = pane:get_current_working_dir()
+  if cwd_uri then
+    local cwd = ''
+    local hostname = ''
+
+    if type(cwd_uri) == 'userdata' then
+      -- Running on a newer version of wezterm and we have
+      -- a URL object here, making this simple!
+
+      cwd = cwd_uri.file_path
+      hostname = cwd_uri.host or wezterm.hostname()
+    else
+      -- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+      -- which doesn't have the Url object
+      cwd_uri = cwd_uri:sub(8)
+      local slash = cwd_uri:find '/'
+      if slash then
+        hostname = cwd_uri:sub(1, slash - 1)
+        -- and extract the cwd from the uri, decoding %-encoding
+        cwd = cwd_uri:sub(slash):gsub('%%(%x%x)', function(hex)
+          return string.char(tonumber(hex, 16))
+        end)
+      end
+    end
+
+    -- Remove the domain name portion of the hostname
+    local dot = hostname:find '[.]'
+    if dot then
+      hostname = hostname:sub(1, dot - 1)
+    end
+    if hostname == '' then
+      hostname = wezterm.hostname()
+    end
+
+    table.insert(cells, cwd)
+    table.insert(cells, hostname)
+  end
+
+  -- I like my date/time in this style: "Wed Mar 3 08:14"
+  local date = wezterm.strftime '%a %b %-d %H:%M'
+  table.insert(cells, date)
+
+  -- An entry for each battery (typically 0 or 1 battery)
+  -- for _, b in ipairs(wezterm.battery_info()) do
+  --   table.insert(cells, string.format('%.0f%%', b.state_of_charge * 100))
+  -- end
+
+  -- The powerline < symbol
+  local LEFT_ARROW = utf8.char(0xe0b3)
+  -- The filled in variant of the < symbol
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+  -- Color palette for the backgrounds of each cell
+  local colors = {
+    '#3c1361',
+    '#52307c',
+    '#663a82',
+    '#7c5295',
+    '#b491c8',
+  }
+
+  -- Foreground color for the text across the fade
+  local text_fg = '#c0c0c0'
+
+  -- The elements to be formatted
+  local elements = {}
+  -- How many cells have been formatted
+  local num_cells = 0
+
+  -- Translate a cell into elements
+  function push(text, is_last)
+    local cell_no = num_cells + 1
+    table.insert(elements, { Foreground = { Color = text_fg } })
+    table.insert(elements, { Background = { Color = colors[cell_no] } })
+    table.insert(elements, { Text = ' ' .. text .. ' ' })
+    if not is_last then
+      table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
+      table.insert(elements, { Text = SOLID_LEFT_ARROW })
+    end
+    num_cells = num_cells + 1
+  end
+
+  while #cells > 0 do
+    local cell = table.remove(cells, 1)
+    push(cell, #cells == 0)
+  end
+
+  window:set_right_status(wezterm.format(elements))
+end)
+
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Dracura+'
+  else
+    return 'Builtin Solarized Light'
+  end
+end
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+-- update
+config.check_for_updates = true
+config.check_for_updates_interval_seconds = 86400
+
+-- scroll backline
+config.scrollback_lines = 35000
+
+-- exit
+config.exit_behavior = 'Hold'
+
+-- boot the full-screen when the terminal starts
+local mux = wezterm.mux
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():toggle_fullscreen()
+end)
+
+--- use IME
+config.use_ime = true
+config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
+
+-- Font settings
+config.font = wezterm.font('Moralerspace Argon HW', 
+  { 
+    stretch = 'Normal',
+    weight = 'Regular',
+    bold = false,
+    italic = false,
+  }
+)
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.text_background_opacity = 0.95
+config.font_size = 15.0
+config.cell_width = 1.0
+config.line_height = 1.0
+config.use_cap_height_to_scale_fallback_fonts = true
+
+-- This increases color saturation by 50%
+config.foreground_text_hsb = {
+  hue = 1.0,
+  saturation = 1.0,
+  brightness = 1.2,
+}
+
+-- Background opacity
+config.window_background_opacity = 0.80
+config.macos_window_background_blur = 10
+
+-- Color Scheme
+-- config.color_scheme = 'Pastel White (terminal.sexy)'
+-- config.color_scheme = 'Dracra+'
+config.color_scheme = scheme_for_appearance(get_appearance())
+
+config.window_frame = {
+  -- The font used in the tab bar.
+  -- Roboto Bold is the default; this font is bundled
+  -- with wezterm.
+  -- Whatever font is selected here, it will have the
+  -- main font setting appended to it to pick up any
+  -- fallback fonts you may have used there.
+  font = wezterm.font { family = 'Cica', weight = 'Bold' },
+
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 14.0,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  active_titlebar_bg = '#333333',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = '#333333',
+}
+
+config.window_padding = {
+  left = 48,
+  right = 48,
+  top = 10,
+  bottom = 24,
+ }
+
+config.colors = {
+  tab_bar = {
+    -- The color of the inactive tab bar edge/divider
+    inactive_tab_edge = '#575757',
+  },
+}
+
+
+-- Leader key
+config.leader = {
+ key = ']',
+ mods = 'CTRL',
+ timeout_milliseconds = 2000,
+}
+
+-- Key bindings
+config.keys = {
+  {
+    key = 'x',
+    mods = 'LEADER',
+    action = act.ActivateCopyMode,
+  },
+  {
+    key = ',',
+    mods = 'SUPER',
+    action = act.QuickSelect,
+  },
+  {
+    key = ',',
+    mods = 'LEADER',
+    action = act.SplitHorizontal {domain = "CurrentPaneDomain"},
+  },
+  {
+    key = '.',
+    mods = 'LEADER',
+    action = act.SplitVertical {domain = "CurrentPaneDomain"},
+  },
+  {
+    key = ']',
+    mods = 'SUPER',
+    action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = '[',
+    mods = 'SUPER',
+    action = act.ActivatePaneDirection 'Left',
+  },  
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = act.CloseCurrentPane { confirm = true },
+  },
+}
+
+return config
