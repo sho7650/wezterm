a837da4c988f97608edfd44733890cca502c7e7c
Takashi Abe <sho@satomi.local>
2024-06-09 21:33:03 +0900

    Adjust color scheme for Dell S2722QC

--- wezterm.lua
+++ wezterm.lua
@@ -133,7 +133,7 @@ end
 
 function scheme_for_appearance(appearance)
   if appearance:find 'Dark' then
-    return 'nord'
+      return 'Nightfly (Gogh)'
   else
       return 'Light White (terminal.sexy)'
   end
